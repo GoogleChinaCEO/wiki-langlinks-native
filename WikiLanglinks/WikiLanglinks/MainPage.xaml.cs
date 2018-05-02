1ef02dd8b719227bc5022e85b68d526d0b54a3da
Igor Redchuk <redchuk@gmail.com>
2018-05-02 12:27:44 +0300

    Create project

---
+++ WikiLanglinks/WikiLanglinks/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace WikiLanglinks
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
