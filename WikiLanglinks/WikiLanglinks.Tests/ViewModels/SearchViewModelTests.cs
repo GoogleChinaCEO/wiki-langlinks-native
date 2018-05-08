6a960900b544720e24ba5b83043468d2844ad17e
Igor Redchuk <redchuk@gmail.com>
2018-05-08 11:07:43 +0200

    Add tests for SearchViewModel

---
+++ WikiLanglinks/WikiLanglinks.Tests/ViewModels/SearchViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.Forms;
+
+namespace WikiLanglinks.Tests.ViewModels
+{
+	[TestFixture]
+	public class SearchViewModelTests
+    {
+		private SearchViewModel _viewModel;
+
+		[SetUp]
+		public void Setup()
+		{
+			_viewModel = new SearchViewModel();
+		}
+
+		[Test]
+        public void WhenSearchCommandIsExecuted_SearchRequestedEventIsSent()
+		{
+			var eventsSent = 0;
+			MessagingCenter.Subscribe<SearchViewModel>(this, EventNames.SearchRequested, s => { eventsSent++; });
+
+			_viewModel.SearchCommand.Execute(null);
+
+			Assert.AreEqual(1, eventsSent);
+		}
+    }
+}
