978275057f8c08b9544a2e5bcede67d5aeaac3a1
Igor Redchuk <redchuk@gmail.com>
2018-05-17 11:51:12 +0200

    Add Xamarin.Essentials package

--- WikiLanglinks/Droid/MainActivity.cs
+++ WikiLanglinks/Droid/MainActivity.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace WikiLanglinks.Droid
@@ -24,8 +20,15 @@ namespace WikiLanglinks.Droid
 			Instance = this;
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Essentials.Platform.Init(this, bundle);
 
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
